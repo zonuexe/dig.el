1f5b535bd0b354629a6126ea524ee79fea5b0b8b
USAMI Kenta <tadsan@zonu.me>
2025-02-11 02:37:36 +0900

    Refactor

--- dig.el
+++ dig.el
@@ -29,7 +29,7 @@
 (require 'cl-lib)
 
 ;; Internal functions
-(defun dig--expand-alist (key subject)
+(defun dig--expand-alist (subject key)
   "Expand an alist lookup expression.
 
 KEY is the key to look up.
@@ -37,7 +37,7 @@ SUBJECT is the alist."
   (let ((test (if (symbolp key) #'eq #'equal)))
     `(alist-get ,key ,subject nil nil (function ,test))))
 
-(defun dig--expand-plist (key subject)
+(defun dig--expand-plist (subject key)
   "Expand a plist lookup expression.
 
 KEY is the key to look up.
@@ -45,31 +45,31 @@ SUBJECT is the plist."
   (let* ((test (if (symbolp key) #'eq #'equal)))
     `(plist-get ,subject ,key (function ,test))))
 
-(defun dig--expand-hash (key subject)
+(defun dig--expand-hash (subject key)
   "Expand a hash table lookup expression.
 
 KEY is the key to look up.
 SUBJECT is the hash table."
   `(when ,subject (gethash ,key ,subject)))
 
-(defun dig--expand-nth (key subject)
+(defun dig--expand-nth (subject key)
   "Expand an indexed list lookup expression.
 
 KEY is the index to retrieve.
 SUBJECT is the list."
   `(nth ,key ,subject))
 
-(defun dig--expand-dwim (key subject)
+(defun dig--expand-dwim (subject key)
   "Expand a lookup expression based on the type of KEY.
 
 KEY is the key to look up.
 SUBJECT is the data structure.
 Expands to the appropriate lookup form based on the type of KEY."
   (cond
-   ((stringp key) (dig--expand-hash key subject))
-   ((integerp key) (dig--expand-nth key subject))
-   ((keywordp key) (dig--expand-plist key subject))
-   ((dig--expand-alist key subject))))
+   ((stringp key) (dig--expand-hash subject key))
+   ((integerp key) (dig--expand-nth subject key))
+   ((keywordp key) (dig--expand-plist subject key))
+   ((dig--expand-alist subject key))))
 
 ;; Macros
 (defmacro dig-alist (subject first-key &rest keys)
@@ -79,10 +79,7 @@ SUBJECT is the initial alist.
 FIRST-KEY is the first key to retrieve.
 KEYS is an optional sequence of additional keys to traverse the alist.
 Returns the value associated with the final key."
-  (let ((result subject))
-    (cl-loop for key in (cons first-key keys)
-             do (setq result (dig--expand-alist key result)))
-    result))
+  (cl-reduce #'dig--expand-alist (cons first-key keys) :initial-value subject))
 
 (defmacro dig-plist (subject first-key &rest keys)
   "Retrieve a nested value from a plist (property list).
@@ -91,10 +88,7 @@ SUBJECT is the initial plist.
 FIRST-KEY is the first key to retrieve.
 KEYS is an optional sequence of additional keys to traverse the plist.
 Returns the value associated with the final key."
-  (let ((result subject))
-    (cl-loop for key in (cons first-key keys)
-             do (setq result (dig--expand-plist key result)))
-    result))
+  (cl-reduce #'dig--expand-plist (cons first-key keys) :initial-value subject))
 
 (defmacro dig-hash (subject first-key &rest keys)
   "Retrieve a nested value from a hash table.
@@ -103,10 +97,7 @@ SUBJECT is the initial hash table.
 FIRST-KEY is the first key to retrieve.
 KEYS is an optional sequence of additional keys to traverse the hash table.
 Returns the value associated with the final key."
-  (let ((result subject))
-    (cl-loop for key in (cons first-key keys)
-             do (setq result (dig--expand-hash key result)))
-    result))
+  (cl-reduce #'dig--expand-hash (cons first-key keys) :initial-value subject))
 
 (defmacro dig-nth (subject first-key &rest keys)
   "Retrieve a nested value from a list using indexed access.
@@ -115,10 +106,7 @@ SUBJECT is the initial list.
 FIRST-KEY is the first index to retrieve.
 KEYS is an optional sequence of additional indices to traverse the list.
 Returns the value at the final index."
-  (let ((result subject))
-    (cl-loop for key in (cons first-key keys)
-             do (setq result (dig--expand-nth key result)))
-    result))
+  (cl-reduce #'dig--expand-nth (cons first-key keys) :initial-value subject))
 
 (defmacro dig (subject first-key &rest keys)
   "Expand a nested lookup expression based on the type of each key.
@@ -128,10 +116,7 @@ FIRST-KEY is the first key to retrieve.
 KEYS is an optional sequence of additional keys or indices, each of which
 determines how to expand its lookup expression at macro expansion time.
 Returns the value associated with the final key or index."
-  (let ((result subject))
-    (cl-loop for key in (cons first-key keys)
-             do (setq result (dig--expand-dwim key result)))
-    result))
+  (cl-reduce #'dig--expand-dwim (cons first-key keys) :initial-value subject))
 
 (provide 'dig)
 ;;; dig.el ends here
